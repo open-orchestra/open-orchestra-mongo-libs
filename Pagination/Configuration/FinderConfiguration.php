0281e37f9fca574b4932b4f31591516cb1f68740
romain boessel <romain.boessel@businessecision.com>
2015-07-03 11:55:55 +0200

    add configuration

---
+++ Pagination/Configuration/FinderConfiguration.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace OpenOrchestra\Pagination\Configuration;
+
+use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * Class FinderConfiguration
+ */
+class FinderConfiguration
+{
+    protected $descriptionEntity = null;
+    protected $columns = null;
+    protected $search = null;
+
+    /**
+     * @param Request $request
+     *
+     * @return FinderConfiguration
+     */
+    public static function generateFromRequest(Request $request)
+    {
+        $finderConfig = new FinderConfiguration();
+        $columns = $request->get('columns');
+        if(FinderConfiguration::isArrayOrNull($columns))
+            $finderConfig->setColumns($columns);
+        $finderConfig->setSearch($request->get('search'));
+
+        return $finderConfig;
+    }
+
+    /**
+     * @param string $value
+     *
+     * @return boolean
+     */
+    public static function isArrayOrNull($value)
+    {
+        return  is_array($value) || $value === NULL;
+    }
+
+    /**
+     * @return array descriptionEntity
+     */
+    public function getDescriptionEntity()
+    {
+        return $this->descriptionEntity;
+    }
+
+    /**
+     * @param null|array $descriptionEntity
+     */
+    public function setDescriptionEntity($descriptionEntity)
+    {
+        $this->descriptionEntity = $descriptionEntity;
+    }
+
+    /**
+     * @return array columns
+     */
+    public function getColumns()
+    {
+        return $this->columns;
+    }
+
+    /**
+     * @param null|array $columns
+     */
+    public function setColumns($columns)
+    {
+        $this->columns = $columns;
+    }
+
+    /**
+     * @return string search
+     */
+    public function getSearch()
+    {
+        return $this->search;
+    }
+
+    /**
+     * @param string $search
+     */
+    public function setSearch($search)
+    {
+        $this->search = $search;
+    }
+}
---
+++ README.md
@@ -0,0 +1,10 @@
+open-orchestra-libs
+===================
+
+| Service       | Badge         |
+| ------------- |:-------------:|
+| Travis | [![Build Status](https://travis-ci.org/open-orchestra/open-orchestra-libs.svg)](https://travis-ci.org/open-orchestra/open-orchestra-libs) |
+| CodeClimate (quality) | [![Code Climate](https://codeclimate.com/github/open-orchestra/open-orchestra-libs/badges/gpa.svg)](https://codeclimate.com/github/open-orchestra/open-orchestra-libs) |
+| CodeClimate (coverage) | [![Test Coverage](https://codeclimate.com/github/open-orchestra/open-orchestra-libs/badges/coverage.svg)](https://codeclimate.com/github/open-orchestra/open-orchestra-libs/coverage) |
+| Sensio Insight | [![SensioLabsInsight](https://insight.sensiolabs.com/projects/e7fb8f90-e8f0-4da4-9375-42e699038b79/big.png)](https://insight.sensiolabs.com/projects/e7fb8f90-e8f0-4da4-9375-42e699038b79) |
+| VersionEye | [![Dependency Status](https://www.versioneye.com/user/projects/551e87a7971f781c480001aa/badge.svg?style=flat)](https://www.versioneye.com/user/projects/551e87a7971f781c480001aa) |
