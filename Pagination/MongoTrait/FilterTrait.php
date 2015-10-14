3da8768d9dba34be549ec30ae0de4fc91aed7583
Nicolas ANNE <nicolas.anne@businessdecision.com>
2015-10-14 12:13:22 +0200

    fix order in DataTable with entity containing embbed

--- Pagination/MongoTrait/FilterTrait.php
+++ Pagination/MongoTrait/FilterTrait.php
@@ -5,6 +5,7 @@ namespace OpenOrchestra\Pagination\MongoTrait;
 use OpenOrchestra\Pagination\Configuration\FinderConfiguration;
 use OpenOrchestra\Pagination\MongoTrait\FilterTypeStrategy\FilterTypeManager;
 use Solution\MongoAggregation\Pipeline\Stage;
+use OpenOrchestra\Mapping\Annotations\Search;
 
 /**
  * Trait FilterTrait
@@ -138,6 +139,33 @@ trait FilterTrait
     }
 
     /**
+     * @param array|null  $order
+     * @param array|null  $descriptionEntity
+     *
+     * @return Stage
+     */
+    protected function generateArrayForFilterSort($order = null , $descriptionEntity = null, $returnOrder = true)
+    {
+        if (null !== $order) {
+            $columnsName = $order['name'];
+            if (isset($descriptionEntity[$columnsName])){
+                if($descriptionEntity[$columnsName] instanceof Search) {
+                    $name = $descriptionEntity[$columnsName]->getField();
+                } elseif (isset($descriptionEntity[$columnsName]['field'])) {
+                    $name = $descriptionEntity[$columnsName]['field'];
+                }
+                $dir = $name;
+                if ($returnOrder) {
+                    $dir = ($order['dir'] == 'desc') ? -1 : 1;
+                }
+                return array(str_replace('.', '_', $name) => $dir);
+            }
+        }
+        
+        return array();
+    }
+    
+    /**
      * @param Stage       $qa
      * @param array|null  $order
      * @param array|null  $descriptionEntity
@@ -146,14 +174,7 @@ trait FilterTrait
      */
     protected function generateFilterSort(Stage $qa, $order = null , $descriptionEntity = null)
     {
-        if (null !== $order) {
-            $columnsName = $order['name'];
-            if (isset($descriptionEntity[$columnsName]) && isset($descriptionEntity[$columnsName]['field'])) {
-                $name = $descriptionEntity[$columnsName]['field'];
-                $dir = ($order['dir'] == 'desc') ? -1 : 1;
-                $qa->sort(array($name => $dir));
-            }
-        }
+        $qa->sort($this->generateArrayForFilterSort($order, $descriptionEntity));
 
         return $qa;
     }
