1b63539049b6967e03dffdac06135914dae71731
Nicolas ANNE <nicolas.anne@businessdecision.com>
2016-01-22 10:22:01 +0100

    add reverse transform

--- Helper/LuceneSyntaxToBddSyntax.php
+++ Helper/LuceneSyntaxToBddSyntax.php
@@ -18,7 +18,7 @@ class LuceneSyntaxToBddSyntax implements LuceneSyntaxToBddSyntaxInterface
      *
      * @return string
      */
-	public function transformConditionStringToBddCondition($field, $condition, $count = 0, $aliases = array())
+	public function transformLuceneConditionToBddCondition($field, $condition, $count = 0, $aliases = array())
 	{
 		$result = array();
 		$operator = preg_quote(LuceneSyntaxToBddSyntax::OPERATOR);
@@ -37,7 +37,7 @@ class LuceneSyntaxToBddSyntax implements LuceneSyntaxToBddSyntaxInterface
 		}
 
 		if (count($encapsuledElements[0]) > 0) {
-			$result = $this->transformConditionStringToBddCondition($field, $condition, $count, $aliases);
+			$result = $this->transformLuceneConditionToBddCondition($field, $condition, $count, $aliases);
 		} else {
 		    $result = json_encode($this->transformConditionArrayToMongoCondition($field, $explodeCondition, $condition, $aliases));
 		}
@@ -46,6 +46,35 @@ class LuceneSyntaxToBddSyntax implements LuceneSyntaxToBddSyntaxInterface
 	}
 
     /**
+     * @param string $condition
+     *
+     * @return array|string
+     */
+	public function transformBddConditionToLuceneCondition($field, $conditions)
+	{
+		foreach($conditions as $key => $condition) {
+			if(is_array($condition)){
+				$conditions[$key] = $this->transformBddConditionToLuceneCondition($field, $condition);
+			}
+			if ($key === '$eq' || $key === $field) {
+				$conditions = $conditions[$key];
+				break;
+			} elseif ($key === '$ne') {
+				$conditions = '-'.$conditions[$key];
+				break;
+			} elseif ($key === '$and') {
+				$conditions = '(+'.implode('+', $conditions[$key]).')';
+				$conditions = preg_replace('/\+-/', '-', $conditions);
+				break;
+			} elseif ($key === '$or') {
+				$conditions = '('.implode(' ', $conditions[$key]).')';
+				break;
+			}
+		}
+
+		return $conditions;
+	}
+	/**
      * @param string $regExp
      * @param array  $condition
      * @param array  $aliases
@@ -65,22 +94,22 @@ class LuceneSyntaxToBddSyntax implements LuceneSyntaxToBddSyntaxInterface
 			}
 		}
 
-	    if (count($conditionElements[2]) == 1) {
-	        $result = array($field => array('$eq' => $conditionElements[2][0]));
-	    } else {
-    	    $result = array();
-    		foreach ($conditionElements[1] as $key => $operator) {
-    			if ($operator == '-') {
-    				$conditionElements[2][$key] = array($field => array('$ne' => $conditionElements[2][$key]));
-    				$conditionElements[1][$key] = '+';
-    			} else {
-    			    $conditionElements[2][$key] = array($field => array('$eq' => $conditionElements[2][$key]));
-    			}
-    			if ($operator == '') {
-    				$conditionElements[1][$key] = ' ';
-    			}
-    		}
-    		$operatorType = array_unique($conditionElements[1]);
+	    $result = array();
+	    foreach ($conditionElements[1] as $key => $operator) {
+	        $comparison = '$eq';
+	        if ($operator == '-') {
+	            $comparison = '$ne';
+	            $conditionElements[1][$key] = '+';
+	        } elseif ($operator == '') {
+	            $conditionElements[1][$key] = ' ';
+	        }
+	        if (!is_array($conditionElements[2][$key])) {
+	            $conditionElements[2][$key] = array($field => array($comparison => $conditionElements[2][$key]));
+	        }
+	    }
+		$operatorType = array_unique($conditionElements[1]);
+	    $result = $conditionElements[2][0];
+		if (count($conditionElements[2]) > 1) {
     		if (count($operatorType) == 1) {
     			if ($operatorType[0] == '+') {
     				$result = array('$and' => $conditionElements[2]);
@@ -97,12 +126,15 @@ class LuceneSyntaxToBddSyntax implements LuceneSyntaxToBddSyntaxInterface
     				}
     			}
     		}
-	    }
+		}
 
 		return $result;
 	}
 }
 
 $luceneSyntaxToBddSyntax = new LuceneSyntaxToBddSyntax();
-print_r($luceneSyntaxToBddSyntax->transformConditionStringToBddCondition('keyword', '+(cat:X1 cat:X2)+(author:AAA)+(T1 T2-T3)'));
+$bddCondition = $luceneSyntaxToBddSyntax->transformLuceneConditionToBddCondition('keyword', '+(cat:X1 (+cat:X2+cat:X3))+(author:AAA)+(T1 T2-T3)');
+echo $bddCondition;
+$luceneCondition = $luceneSyntaxToBddSyntax->transformBddConditionToLuceneCondition('keyword', json_decode($bddCondition, true));
+echo $luceneCondition;
 ?>
