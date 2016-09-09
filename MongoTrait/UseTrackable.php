99ccb785462e8ce61bf5e7c8558f4269f28b3ebe
Noël GILAIN <noel.gilain@businessdecision.com>
2016-09-09 17:40:22 +0200

    1.2 keyword references (#50)
    * Add UseTrackable trait
    
    * Add use in Content Type and Media
    
    * Refacto UseTrackable to use generic addReferenceToEntity and removeReferenceToEntity
    
    * Add UseTrackable::isUsed
    
    * Update UseTrackable::getUseReferences to allow context selection

---
+++ MongoTrait/UseTrackable.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace OpenOrchestra\MongoTrait;
+
+/**
+ * Trait UseTrackable
+ */
+trait UseTrackable
+{
+    /**
+     * @ODM\Field(type="hash")
+     */
+    protected $useReferences = array();
+
+    /**
+     * @param string $entityId
+     * @param string $entityType
+     */
+    public function addUseInEntity($entityId, $entityType)
+    {
+        if (is_string($entityId) && is_string($entityType)) {
+            $this->useReferences[$entityType][$entityId] = $entityId;
+        }
+    }
+
+    /**
+     * @param string $entityId
+     * @param string $entityType
+     */
+    public function removeUseInEntity($entityId, $entityType)
+    {
+        if (is_string($entityId) && is_string($entityType)) {
+            unset($this->useReferences[$entityType][$entityId]);
+        }
+    }
+
+    /**
+     * @param string|null $entityType
+     *
+     * @return array
+     */
+    public function getUseReferences($entityType = null)
+    {
+        if (is_null($entityType)) {
+
+            return $this->useReferences;
+        }
+
+        if (isset($this->useReferences[$entityType])) {
+
+            return $this->useReferences[$entityType];
+        }
+
+        return array();
+    }
+
+    /**
+     * @return boolean
+     */
+    public function isUsed()
+    {
+        $countReferences = 0;
+
+        foreach ($this->useReferences as $referencesByType) {
+            if (is_array($referencesByType)) {
+                $countReferences += count($referencesByType);
+            }
+        }
+
+        return 0 < $countReferences;
+    }
+}
