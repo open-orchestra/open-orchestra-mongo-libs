1fd67afc34daf74f5258571730da5b6ad59917b4
Amaury Lavieille <amaury.lavieille@gmail.com>
2017-03-02 14:30:47 +0100

    Remove annotation search (#73)
    * remove annotation search
    
    * update

--- MongoBundle/DependencyInjection/Configuration.php
+++ MongoBundle/DependencyInjection/Configuration.php
@@ -18,23 +18,7 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('open_orchestra_mongo');
-        $rootNode->children()
-                ->arrayNode('search_metadata')
-                    ->addDefaultsIfNotSet()
-                    ->children()
-                        ->scalarNode('cache_dir')->defaultValue('%kernel.cache_dir%/search_annotation')->end()
-                        ->booleanNode('auto_detection')->defaultTrue()->end()
-                        ->arrayNode('directories')
-                            ->useAttributeAsKey('bundle')
-                            ->prototype('array')
-                            ->children()
-                                ->scalarNode('path')->isRequired()->end()
-                                ->scalarNode('namespace_prefix')->defaultValue('')->end()
-                            ->end()
-                        ->end()
-                    ->end()
-                ->end();
+        $treeBuilder->root('open_orchestra_mongo');
 
         // Here you should define the parameters that are allowed to
         // configure your bundle. See the documentation linked above for
